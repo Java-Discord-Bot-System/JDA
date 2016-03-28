d31fb6c845a905d614fbba0b376ca691b4481854
Almighty-Alpaca <Almighty-Alpaca@users.noreply.github.com>
2016-03-28 17:10:09 +0200

    Async event handling is now a thing

--- src/main/java/com/almightyalpaca/discord/bot/system/events/manager/EventHandler.java
+++ src/main/java/com/almightyalpaca/discord/bot/system/events/manager/EventHandler.java
@@ -2,6 +2,7 @@ package com.almightyalpaca.discord.bot.system.events.manager;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -9,18 +10,9 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
+@Inherited
 public @interface EventHandler {
 
-	/**
-	 * Not used yet, might be in the future.
-     * Weather the handler supports async or not. Default is <b>false</b>.
-	 */
 	boolean async() default false;
-
-	/**
-	 * Not used yet, might be in the future.
-	 * The priority of this method. Default is <b>0</b>.
-	 */
-	int priority() default 0;
 
 }
